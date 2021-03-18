26ffdfb2c092bcb7e1622ef85b0d924d1683b217
Janko Radakovic <jankoradakovic@sda-lund-67-mbpro.lan>
2021-03-18 11:28:36 +0100

    Adds fullNameOfPeopleOver35 and ageSum methods

--- submission.js
+++ submission.js
@@ -1,27 +1,13 @@
-// const args = process.argv.splice(2);
+
+const dataSourceArray = process.argv.slice(2);
+
+const dataSource = dataSourceArray[0];
 
 const fs = require("fs");
 
-const data = fs.readFileSync("MOCK_DATA.json");
-const arrayPeople = JSON.parse(data);
+const dataFromFile = fs.readFileSync(dataSource);
 
-
-
-
-
-// const fs = require("fs");
-
-
-
-// fileNames.forEach((fileName) => {
-//   fs.readFile(fileName, "utf-8", (error, data) => {
-//     if (error) throw error;
-//     console.log(fileName, data);
-//   });
-// });
-
-// const fileName = fileNames[0];
-// console.log(fileName, fs.readFileSync(fileName, "utf-8"));
+let personData = JSON.parse(dataFromFile);
 
 function femalesCount(listOfPeople) {
   const femalePersons = listOfPeople.filter(
@@ -30,6 +16,19 @@ function femalesCount(listOfPeople) {
   return femalePersons.length;
 }
 
-console.log("Number of females is: " + femalesCount(arrayPeople))
+function fullNameOfAllPeopleOver35(listOfPeople) {
+  const peopleOver35 = listOfPeople.filter((person) => person.age > 35);
+  return peopleOver35.map(
+    (person) => person.first_name + " " + person.last_name
+  );
+}
 
-module.exports = femalesCount;
+function ageSum(listOfPeople) {
+  return listOfPeople.reduce((total, value) => total + value.age, 0)
+}
+
+console.log("Number of females is: " + femalesCount(personData));
+console.log("Full name of people over 35 are: " + fullNameOfAllPeopleOver35(personData));
+console.log("The sum of all ages is: " + ageSum(personData));
+
+module.exports = {femalesCount: femalesCount, fullNameOfAllPeopleOver35: fullNameOfAllPeopleOver35 }
--- submission.test.js
+++ submission.test.js
@@ -1,18 +1,50 @@
-const femalesCount = require('./submission');
+const submission = require("./submission");
 
-test('femalesCount should count correct number of females', () => {
-  expect(femalesCount([
-    {
-      id: 7, first_name: 'Minne', last_name: 'MacGilfoyle', email: 'mmacgilfoyle6@amazon.co.jp', gender: 'Female', ip_address: '12.246.212.112', age: 54,
-    },
-    {
-      id: 8, first_name: 'Purcell', last_name: 'Mearns', email: 'pmearns7@chicagotribune.com', gender: 'Male', ip_address: '60.16.88.134', age: 24,
-    },
-    {
-      id: 9, first_name: 'Amanda', last_name: 'Gownge', email: 'agownge8@t.co', gender: 'Female', ip_address: '246.160.41.135', age: 95,
-    },
-    {
-      id: 10, first_name: 'Tybie', last_name: 'Tille', email: 'ttille9@cargocollective.com', gender: 'Female', ip_address: '82.56.248.7', age: 30,
-    },
-  ])).toBe(3);
+const listOfPeople = [
+  {
+    id: 7,
+    first_name: "Minne",
+    last_name: "MacGilfoyle",
+    email: "mmacgilfoyle6@amazon.co.jp",
+    gender: "Female",
+    ip_address: "12.246.212.112",
+    age: 54,
+  },
+  {
+    id: 8,
+    first_name: "Purcell",
+    last_name: "Mearns",
+    email: "pmearns7@chicagotribune.com",
+    gender: "Male",
+    ip_address: "60.16.88.134",
+    age: 24,
+  },
+  {
+    id: 9,
+    first_name: "Amanda",
+    last_name: "Gownge",
+    email: "agownge8@t.co",
+    gender: "Female",
+    ip_address: "246.160.41.135",
+    age: 95,
+  },
+  {
+    id: 10,
+    first_name: "Tybie",
+    last_name: "Tille",
+    email: "ttille9@cargocollective.com",
+    gender: "Female",
+    ip_address: "82.56.248.7",
+    age: 30,
+  },
+];
+
+test("femalesCount should count correct number of females", () => {
+  expect(submission.femalesCount(listOfPeople)).toBe(3);
+});
+
+test("fullNameOfAllPeopleOver35 should return first and last name of people over 35", () => {
+  expect(submission.fullNameOfAllPeopleOver35(listOfPeople)).toEqual([ "Minne MacGilfoyle,Amanda Gownge"]
+   
+  );
 });
