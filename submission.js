d8e96f026262753357a591163674634010cd5b96
Janko Radakovic <jankoradakovic@sda-lund-67-mbpro.lan>
2021-03-18 15:19:48 +0100

    Resolves eslint warnings

--- submission.js
+++ submission.js
@@ -1,16 +1,17 @@
-// const dataSourceArray = process.argv.slice(2);
+/* eslint-disable no-console */
+const dataSourceArray = process.argv.slice(2);
 
-// const dataSource = dataSourceArray[0];
+const dataSource = dataSourceArray[0];
 
-// const fs = require("fs");
+const fs = require('fs');
 
-// const dataFromFile = fs.readFileSync(dataSource);
+const dataFromFile = fs.readFileSync(dataSource);
 
-// let personData = JSON.parse(dataFromFile);
+const personData = JSON.parse(dataFromFile);
 
 function femalesCount(listOfPeople) {
   const femalePersons = listOfPeople.filter(
-    (person) => person.gender === "Female"
+    (person) => person.gender === 'Female',
   );
   return femalePersons.length;
 }
@@ -18,7 +19,7 @@ function femalesCount(listOfPeople) {
 function fullNameOfAllPeopleOver35(listOfPeople) {
   const peopleOver35 = listOfPeople.filter((person) => person.age > 35);
   return peopleOver35.map(
-    (person) => person.first_name + " " + person.last_name
+    (person) => `${person.first_name} ${person.last_name}`,
   );
 }
 
@@ -32,41 +33,38 @@ function averageAge(listOfPeople) {
 
 function fullNameOfAllPeopleOver22Under45FirstOrLastWithL(listOfPeople) {
   const youngerThan43OlderThan22 = listOfPeople.filter(
-    (person) => person.age > 22 && person.age < 45
+    (person) => person.age > 22 && person.age < 45,
   );
   const firstOrLastNameStartWithL = youngerThan43OlderThan22.filter(
-    (person) =>
-      person.first_name.charAt(0) === "L" || person.last_name.charAt(0) === "L"
+    (person) => person.first_name.charAt(0) === 'L' || person.last_name.charAt(0) === 'L',
   );
   return firstOrLastNameStartWithL.map(
-    (person) => person.first_name + " " + person.last_name
+    (person) => `${person.first_name} ${person.last_name}`,
   );
 }
 
 function peopleWithGovEmail(listOfPeople) {
   const arrayOfPeopleWtihGovEmail = listOfPeople.filter(
-    (person) =>
-      person.email.slice(person.email.length - 3) === "gov" ||
-      person.email.includes(".gov.")
+    (person) => person.email.slice(person.email.length - 3) === 'gov'
+      || person.email.includes('.gov.'),
   );
   return arrayOfPeopleWtihGovEmail.map(
-    (person) =>
-      person.first_name + " " + person.last_name + " " + person.ip_address
+    (person) => `${person.first_name} ${person.last_name} ${person.ip_address}`,
   );
 }
 
-// console.log("Number of females is: " + femalesCount(personData));
-// console.log("Full name of people over 35 are: " + fullNameOfAllPeopleOver35(personData));
-// console.log("The sum of all ages is: " + ageSum(personData));
-// console.log("The average age is: " + averageAge(personData));
-// console.log("Full name of people over 22, under 45 and first or last name starts with 'L': " + fullNameOfAllPeopleOver22Under45FirstOrLastWithL(personData));
-// console.log("Full name and ip address of people with government email: " + peopleWithGovEmail(personData));
+console.log(`Number of females is: ${femalesCount(personData)}`);
+console.log(`Full name of people over 35 are: ${fullNameOfAllPeopleOver35(personData)}`);
+console.log(`The sum of all ages is: ${ageSum(personData)}`);
+console.log(`The average age is: ${averageAge(personData)}`);
+console.log(`Full name of people over 22, under 45 and first or last name starts with 'L': ${fullNameOfAllPeopleOver22Under45FirstOrLastWithL(personData)}`);
+console.log(`Full name and ip address of people with government email: ${peopleWithGovEmail(personData)}`);
 
 module.exports = {
-  femalesCount: femalesCount,
-  fullNameOfAllPeopleOver35: fullNameOfAllPeopleOver35,
-  ageSum: ageSum,
-  averageAge: averageAge,
-  fullNameOfAllPeopleOver22Under45FirstOrLastWithL: fullNameOfAllPeopleOver22Under45FirstOrLastWithL,
-  peopleWithGovEmail: peopleWithGovEmail,
+  femalesCount,
+  fullNameOfAllPeopleOver35,
+  ageSum,
+  averageAge,
+  fullNameOfAllPeopleOver22Under45FirstOrLastWithL,
+  peopleWithGovEmail,
 };
--- submission.test.js
+++ submission.test.js
@@ -1,71 +1,73 @@
-const submission = require("./submission");
+/* eslint-disable no-undef */
+const submission = require('./submission');
 
 const listOfPeople = [
   {
     id: 7,
-    first_name: "Minne",
-    last_name: "MacGilfoyle",
-    email: "mmacgilfoyle6@amazon.gov.jp",
-    gender: "Female",
-    ip_address: "12.246.212.112",
+    first_name: 'Minne',
+    last_name: 'MacGilfoyle',
+    email: 'mmacgilfoyle6@amazon.gov.jp',
+    gender: 'Female',
+    ip_address: '12.246.212.112',
     age: 54,
   },
   {
     id: 8,
-    first_name: "Purcell",
-    last_name: "Learns",
-    email: "pmearns7@chicagotribunegov.com",
-    gender: "Male",
-    ip_address: "60.16.88.134",
+    first_name: 'Purcell',
+    last_name: 'Learns',
+    email: 'pmearns7@chicagotribunegov.com',
+    gender: 'Male',
+    ip_address: '60.16.88.134',
     age: 24,
   },
   {
     id: 9,
-    first_name: "Amanda",
-    last_name: "Gownge",
-    email: "agowngov8@t.co",
-    gender: "Female",
-    ip_address: "246.160.41.135",
+    first_name: 'Amanda',
+    last_name: 'Gownge',
+    email: 'agowngov8@t.co',
+    gender: 'Female',
+    ip_address: '246.160.41.135',
     age: 95,
   },
   {
     id: 10,
-    first_name: "Lybie",
-    last_name: "Tille",
-    email: "ttille9@cargocollective.gov",
-    gender: "Female",
-    ip_address: "82.56.248.7",
+    first_name: 'Lybie',
+    last_name: 'Tille',
+    email: 'ttille9@cargocollective.gov',
+    gender: 'Female',
+    ip_address: '82.56.248.7',
     age: 30,
   },
 ];
 
-test("femalesCount should count correct number of females", () => {
+test('femalesCount should count correct number of females', () => {
   expect(submission.femalesCount(listOfPeople)).toBe(3);
 });
 
-test("fullNameOfAllPeopleOver35 should return first and last name of people over 35", () => {
+test('fullNameOfAllPeopleOver35 should return first and last name of people over 35', () => {
+  // eslint-disable-next-line no-undef
   expect(submission.fullNameOfAllPeopleOver35(listOfPeople)).toEqual([
-    "Minne MacGilfoyle",
-    "Amanda Gownge",
+    'Minne MacGilfoyle',
+    'Amanda Gownge',
   ]);
 });
 
-test("ageSum should return sum of all ages", () => {
+test('ageSum should return sum of all ages', () => {
   expect(submission.ageSum(listOfPeople)).toBe(203);
 });
 
-test("avaregeAge should return average age of all people", () => {
+test('avaregeAge should return average age of all people', () => {
   expect(submission.averageAge(listOfPeople)).toBe(50.75);
 });
 
 test("fullNameOfAllPeopleOver22Under45FirstOrLastWithL should return first and last name of people over 22, under 45 and who's first or last name starts with 'L'", () => {
   expect(
-    submission.fullNameOfAllPeopleOver22Under45FirstOrLastWithL(listOfPeople)
-  ).toEqual(["Purcell Learns", "Lybie Tille"]);
+    submission.fullNameOfAllPeopleOver22Under45FirstOrLastWithL(listOfPeople),
+  ).toEqual(['Purcell Learns', 'Lybie Tille']);
 });
 
-test("peopleWithGovEmail should return first, last name and ip address of people with gov email ", () => {
+test('peopleWithGovEmail should return first, last name and ip address of people with gov email ', () => {
   expect(
-    submission.peopleWithGovEmail(listOfPeople)
-  ).toEqual(["Minne MacGilfoyle 12.246.212.112", "Lybie Tille 82.56.248.7"]);
+    submission.peopleWithGovEmail(listOfPeople),
+  ).toEqual(['Minne MacGilfoyle 12.246.212.112', 'Lybie Tille 82.56.248.7']);
 });
